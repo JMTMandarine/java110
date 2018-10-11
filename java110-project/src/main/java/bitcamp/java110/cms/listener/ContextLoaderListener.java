8d2f0ef1fa5e17f7cf41dfb75d5f28507b3545fd
taehyeong <leeth4115@naver.com>
2018-10-11 09:51:44 +0900

    서비스 컴포넌트 적용

--- java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
+++ java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
@@ -9,6 +9,8 @@ import bitcamp.java110.cms.dao.impl.MemberMysqlDao;
 import bitcamp.java110.cms.dao.impl.PhotoMysqlDao;
 import bitcamp.java110.cms.dao.impl.StudentMysqlDao;
 import bitcamp.java110.cms.dao.impl.TeacherMysqlDao;
+import bitcamp.java110.cms.service.AuthService;
+import bitcamp.java110.cms.service.impl.AuthServiceImpl;
 import bitcamp.java110.cms.service.impl.ManagerServiceImpl;
 import bitcamp.java110.cms.service.impl.StudentServiceImpl;
 import bitcamp.java110.cms.service.impl.TeacherServiceImpl;
@@ -47,6 +49,8 @@ public class ContextLoaderListener implements ServletContextListener {
             TeacherMysqlDao teacherDao = new TeacherMysqlDao();
             teacherDao.setDataSource(dataSource);
             
+            
+            
             //서비스 객체 준비
             ManagerServiceImpl managerService=new ManagerServiceImpl();
             managerService.setMemberDao(memberDao);
@@ -62,10 +66,17 @@ public class ContextLoaderListener implements ServletContextListener {
             teacherService.setMemberDao(memberDao);
             teacherService.setTeacherDao(teacherDao);;
             teacherService.setPhotoDao(photoDao);
+            
+            AuthServiceImpl authService = new AuthServiceImpl();
+            authService.setManagerDao(managerDao);
+            authService.setStudentDao(studentDao);
+            authService.setTeacherDao(teacherDao);
+            
             // 서블릿에서 Service를 이용할 수 있도록 ServletContext 보관소에 저장하기
             sc.setAttribute("managerService", managerService);
             sc.setAttribute("studentService", studentService);
             sc.setAttribute("teacherService", teacherService);
+            sc.setAttribute("authService", authService);
             
         } catch (Exception e) {
             e.printStackTrace();
