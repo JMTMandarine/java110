3c011176587a1473750f1730afbd283dbe3519c5
taehyeong <leeth4115@naver.com>
2018-10-23 11:27:46 +0900

    @RequestParam으로 클라이언트가 보낸 값 자동주입

--- java110-project/src/main/java/bitcamp/java110/cms/web/TeacherListController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/web/TeacherListController.java
@@ -1,6 +1,7 @@
 package bitcamp.java110.cms.web;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import bitcamp.java110.cms.domain.Teacher;
 import bitcamp.java110.cms.mvc.RequestMapping;
+import bitcamp.java110.cms.mvc.RequestParam;
 import bitcamp.java110.cms.service.TeacherService;
 
 @Component
@@ -19,52 +21,43 @@ public class TeacherListController {
 
     @Autowired
     TeacherService teacherService;
-    
+
     @Autowired
     ServletContext sc;
-    
+
     @RequestMapping("/teacher/list")
     public String list(
-            HttpServletRequest request) 
-            throws Exception {
-        
-        int pageNo=1;
-        int pageSize=10;
-        
-        if(request.getParameter("pageNo")!=null) {
-            pageNo = Integer.parseInt(request.getParameter("pageNo"));
-            if(pageNo<1) {
-                pageNo=1;
-            }
+            @RequestParam(value="pageNo", defaultValue="1") int pageNo,
+            @RequestParam(value="pageSize", defaultValue="5") int pageSize,
+            Map<String,Object> map) 
+                    throws Exception {
+
+        if(pageNo<1) {
+            pageNo=1;
         }
-        
-        if(request.getParameter("pageSize")!=null) {
-            pageSize = Integer.parseInt(request.getParameter("pageSize"));
-            if(pageSize < 3 || pageSize>=10) {
-                pageSize=3;
-            }
+        if(pageSize < 3 || pageSize>=10) {
+            pageSize=3;
         }
-        
+
         List<Teacher> list = teacherService.list(pageNo, pageSize);
-        request.setAttribute("list", list);
+        map.put("list", list);
         return "/teacher/list.jsp";
     }
-    
+
     @RequestMapping("/teacher/detail")
     public String detail(
-            HttpServletRequest request) 
-            throws Exception {
+            @RequestParam(value="no") int no,
+            Map<String,Object> map) 
+                    throws Exception {
 
-        int no = Integer.parseInt(request.getParameter("no"));
-        
-        
         Teacher t = teacherService.get(no);
-        request.setAttribute("teacher", t);
+        map.put("teacher", t);
         return "/teacher/detail.jsp";
     }
-    
+
     @RequestMapping("/teacher/add")
     public String add(
+            Teacher teacher,
             HttpServletRequest request) 
                     throws Exception {
 
@@ -72,37 +65,22 @@ public class TeacherListController {
             return "/teacher/form.jsp";
         }
 
-
-        request.setCharacterEncoding("UTF-8");
-
-        Teacher t = new Teacher();
-        t.setName(request.getParameter("name"));
-        t.setEmail(request.getParameter("email"));
-        t.setPassword(request.getParameter("password"));
-        t.setTel(request.getParameter("tel"));
-        t.setPay(Integer.parseInt(request.getParameter("pay")));
-        t.setSubjects(request.getParameter("subjects"));
-
-
         Part part = request.getPart("file1");
         if (part.getSize() > 0) {
             String filename = UUID.randomUUID().toString(); //고유파일명을 가짐.
             part.write(sc.getRealPath("/upload/" + filename));
-            t.setPhoto(filename);
+            teacher.setPhoto(filename);
         }
-        teacherService.add(t);
+        teacherService.add(teacher);
         return "redirect:list";
 
 
     }
-    
+
     @RequestMapping("/teacher/delete")
     public String delete(
-            HttpServletRequest request) 
+            @RequestParam("no") int no) 
                     throws Exception {
-
-        int no = Integer.parseInt(request.getParameter("no"));
-
 
         teacherService.delete(no);
         return "redirect:list";
